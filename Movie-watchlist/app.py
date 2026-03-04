58a4fc06a82e30152e0459a39988cc79f631045b
nyossick <nyossick@gmu.edu>
2026-03-03 21:14:21 -0500

    fixed a bug not allowing for items to be put in watchlist but for real this time v6

--- Movie-watchlist/app.py
+++ Movie-watchlist/app.py
@@ -8,6 +8,10 @@ from psycopg2.extras import RealDictCursor
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "watchlist.db")
 load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+USE_POSTGRES = DATABASE_URL is not None
+print(f"DEBUG DATABASE_URL: {os.getenv('DATABASE_URL')}")
+print(f"DEBUG USE_POSTGRES: {USE_POSTGRES}")
 
 
 app = Flask(__name__)
